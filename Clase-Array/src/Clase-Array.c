26727aa3c3026a66d6206420db7eae183f2003e5
ornecurcio <ornecurcio@gmail.com>
2021-04-02 17:44:58 -0300

    imprimir array

--- Clase-Array/src/Clase-Array.c
+++ Clase-Array/src/Clase-Array.c
@@ -16,6 +16,9 @@
 #include "numeros.h"
 
 #define EDADESLARGO 5
+
+void imprimirArray(int listaDeEdades[]);
+
 int main(void) {
 	setbuf(stdout, NULL);
 
@@ -30,10 +33,35 @@ int main(void) {
 			edades[i]=edad;
 		}
 	}
+	imprimirArray(edades);
 
-	for(i=0; i<EDADESLARGO; i++)
-	{
-		printf("%d ", edades[i]);
-	}
 	return EXIT_SUCCESS;
 }
+void imprimirArray(int listaDeEdades[])
+{	int i;
+	for(i=0; i<EDADESLARGO; i++)
+	{
+		printf("%d ", listaDeEdades[i]);
+	}
+}
+// El nombre del array es la direccion de memoria donde comienza el array
+
+/* void imprimirArray(int array[]) //es por referencia.
+ * {
+ * 	int j;
+ * 	array[2]=3; // aca lee
+ * 	j = array[3]+7; // aca escribe
+ * }
+ */
+/*void imprimirArray(int* e)
+ * {
+ * 		e=9;    //pasaje por referencia; leo o escribo el valor
+ * 	}
+ *
+ * void imprimirArray(int e)
+ * {
+ *     e=9; pasaje por valor; no puedo escribir el valor, solo leer
+ *
+ *     e+7 = j;
+ * }
+ */
